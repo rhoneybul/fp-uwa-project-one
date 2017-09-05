39477027c62194d93672df11b6500790623ff51d
rhoneybul <robert.honeybul@rthdatasolutions.com>
2017-09-05 16:27:42 +0800

    first commit

---
+++ data-collection/build.sbt
@@ -0,0 +1,10 @@
+scalaVersion := "2.12.3"
+
+name := "data-collection"
+version := "1.0"
+
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats" % "0.9.0",
+  "org.jsoup" %  "jsoup" % "1.6.1"
+)
+
---
+++ data-collection/src/main/scala/Main.scala
@@ -0,0 +1,101 @@
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import scala.collection.JavaConversions._
+import java.io._
+
+object Main extends App {
+
+  def getDataFromLink(
+    link : String,
+    filename : String,
+    curr_index : Int,
+    total_index : Int,
+    fileWriter : PrintWriter
+  ) = {
+
+    println(s"Getting Data for: ${link}, Link: ${curr_index} of ${total_index}")
+    doc = Jsoup.connect(link).get()
+    var tables = doc.select("table.wikitable")
+
+    val yearSplit = filename.split(" ")
+    var year = yearSplit(yearSplit.length-1)
+
+    var table = tables(0)
+
+    // GET FOR ALL TABLES, NOT JUST THE FIRST
+
+    var headers = table.select("tr > th").map(
+      header => header.text
+    ).zipWithIndex.filter(
+      header => relevantHeaders.contains(header._1)
+    )
+
+    var headerIndices = headers.map(header => header._2)
+    var headerVals = headers.map(header => header._1.replace("\n", ""))
+
+    var rows = table.select("tbody > tr").zipWithIndex
+    rows.remove(0)
+
+    val allRows = rows.map(
+      row => row._1.select("td").map(
+        el => el.text
+      ).zipWithIndex.filter(
+        rowElem => headerIndices.contains(rowElem._2)
+      )
+    )
+
+    val rowValues = allRows.map(row => row.map(el => el._1.replace("\n", "")))
+
+    Writer.write("Year".concat("\t"))
+    Writer.write(headerVals.mkString("\t").concat("\n"))
+    rowValues.map(
+      row => Writer.write(year.concat("\t").concat(row.mkString("\t").concat("\n")))
+    )
+    Writer.write("\n")
+  }
+
+  val wikiBaseUrl = "http://en.wikipedia.org"
+
+  var doc = Jsoup.connect(
+    "https://en.wikipedia.org/wiki/List_of_terrorist_incidents"
+  ).get()
+
+  val pageLinks = doc.select(".div-col > ul > li > a")
+  val years = List.range(1970, 2018)
+  val relevantHeaders : List[String] = List(
+    "Type",
+    "Dead",
+    "Date",
+    "Injured",
+    "Location",
+    "Details",
+    "Perpetrator"
+  )
+
+  val yearsAsString = years.map(year => year.toString)
+
+  val relevantLinks = for(
+    link <- pageLinks;
+    if yearsAsString.map(
+      year => link.text.contains(year)
+    ).contains(true)) yield (link.text, wikiBaseUrl.concat(link.attr("href")))
+
+  val relevantLinksWithIndex = relevantLinks.zipWithIndex
+
+  val Writer = new PrintWriter(new File("data/incidents.txt"))
+
+  relevantLinksWithIndex.map(
+    link => getDataFromLink(
+      link._1._2,
+      link._1._1,
+      link._2,
+      relevantLinksWithIndex.length,
+      Writer
+    )
+  )
+
+  Writer.close()
+
+}
